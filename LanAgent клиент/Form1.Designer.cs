9d9dc590a1e7ead6a14ed76260aec5dc65ba5c60
Your Name <you@example.com>
2022-06-16 20:25:16 +0300

    Add project files.

---
+++ LanAgent клиент/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace LanAgent_клиент
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxAdress = new System.Windows.Forms.TextBox();
+            this.tbxPort = new System.Windows.Forms.TextBox();
+            this.tbxTime = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tslStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.cbxAutorun = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Адрес сервера:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(130, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Порт:";
+            // 
+            // tbxAdress
+            // 
+            this.tbxAdress.Location = new System.Drawing.Point(12, 27);
+            this.tbxAdress.Name = "tbxAdress";
+            this.tbxAdress.Size = new System.Drawing.Size(100, 20);
+            this.tbxAdress.TabIndex = 2;
+            this.tbxAdress.Text = "127.0.0.1";
+            // 
+            // tbxPort
+            // 
+            this.tbxPort.Location = new System.Drawing.Point(133, 27);
+            this.tbxPort.Name = "tbxPort";
+            this.tbxPort.Size = new System.Drawing.Size(100, 20);
+            this.tbxPort.TabIndex = 3;
+            this.tbxPort.Text = "9595";
+            this.tbxPort.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbxPort_KeyPress);
+            // 
+            // tbxTime
+            // 
+            this.tbxTime.Location = new System.Drawing.Point(125, 69);
+            this.tbxTime.Name = "tbxTime";
+            this.tbxTime.Size = new System.Drawing.Size(32, 20);
+            this.tbxTime.TabIndex = 4;
+            this.tbxTime.Text = "10";
+            this.tbxTime.TextChanged += new System.EventHandler(this.tbxTime_TextChanged);
+            this.tbxTime.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbxPort_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Отправлять каждые";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(163, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "мин.";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tslStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 109);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(366, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tslStatus
+            // 
+            this.tslStatus.Name = "tslStatus";
+            this.tslStatus.Size = new System.Drawing.Size(118, 17);
+            this.tslStatus.Text = "toolStripStatusLabel1";
+            // 
+            // cbxAutorun
+            // 
+            this.cbxAutorun.AutoSize = true;
+            this.cbxAutorun.Location = new System.Drawing.Point(239, 29);
+            this.cbxAutorun.Name = "cbxAutorun";
+            this.cbxAutorun.Size = new System.Drawing.Size(96, 17);
+            this.cbxAutorun.TabIndex = 5;
+            this.cbxAutorun.Text = "Автозагрузка";
+            this.cbxAutorun.UseVisualStyleBackColor = true;
+            this.cbxAutorun.CheckedChanged += new System.EventHandler(this.cbxAutorun_CheckedChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 600000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 131);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbxAutorun);
+            this.Controls.Add(this.tbxTime);
+            this.Controls.Add(this.tbxPort);
+            this.Controls.Add(this.tbxAdress);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LanAgent (клиент)";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxAdress;
+        private System.Windows.Forms.TextBox tbxPort;
+        private System.Windows.Forms.TextBox tbxTime;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tslStatus;
+        private System.Windows.Forms.CheckBox cbxAutorun;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ LanAgent клиент/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DllStruct;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Net.Sockets;
+using System.Diagnostics;
+using Microsoft.Win32;
+using System.Drawing.Imaging;
+
+namespace LanAgent_клиент
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static byte[] StructToByte(info comp)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream();
+            bf.Serialize(ms, comp);
+            return ms.ToArray();
+        }
+
+        private void Connect(string server,int port,info comp)
+        {
+            try
+            {
+                TcpClient client = new TcpClient(server, port);
+                NetworkStream stream = client.GetStream();
+                byte[] data = StructToByte(comp);
+
+                stream.Write(data, 0, data.Length);
+
+                stream.Close();
+                client.Close();
+                tslStatus.Text = DateTime.Now + " данные на сервер успешно отправлены";
+            }
+            catch
+            {
+                tslStatus.Text = DateTime.Now + " ошибка отправки данных на сервер";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            info comp;
+            //список запущенных процессов
+            Process[] prc = Process.GetProcesses();
+            // сформировать массив строк, в котором перечисляются именя процессов
+            // и объем памяти в килобайтах, который они занимают
+            comp.process = new string[prc.Length];
+            for (int i = 0; i <= prc.Length - 1; i++)
+                comp.process[i] = prc[i].ProcessName + " / " + prc[i].PrivateMemorySize64 / 1024;
+            //список логических дисков
+            comp.allDrives = DriveInfo.GetDrives();
+            //имя пользователя
+            comp.username = Environment.UserName;
+            // сетевое имя компьютера
+            comp.netname = Environment.MachineName;
+            // текущая дата и время
+            comp.dt = DateTime.Now;
+            // IP-адрес компьютера
+            comp.ip = System.Net.Dns.GetHostEntry(Environment.MachineName).AddressList[0].ToString();
+            //скриншоты
+            Bitmap bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics graph = Graphics.FromImage(bmp);
+            graph.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+            // поток в ОЗУ
+            MemoryStream ms = new MemoryStream();
+            // сохранить в поток скриншот в формате JPEG
+            bmp.Save(ms, ImageFormat.Jpeg);
+            // поставить указатель на начало потока
+            ms.Position = 0;
+            // сохранить изображение из потока в структуру данных
+            comp.scr = Image.FromStream(ms);
+
+            int port = int.Parse(tbxPort.Text);
+            Connect(tbxAdress.Text, port, comp);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            RegistryKey k = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\LanAgentSettings");
+            k.SetValue("IpAdress", tbxAdress.Text);
+            k.SetValue("Port", tbxPort.Text);
+            k.SetValue("Time", tbxTime.Text);
+            k.Close();
+        }
+
+        private void tbxPort_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar)) e.Handled = true;
+            if(e.KeyChar == (char)Keys.Back) e.Handled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run");
+            cbxAutorun.Checked = key.GetValue("LanAgent") != null;
+
+            String[] arg = Environment.GetCommandLineArgs();
+            if(arg.Length >1)
+                if(arg[1] == "/min")
+                {
+                    this.Hide();
+                    ShowInTaskbar = false;
+                    WindowState = FormWindowState.Minimized;
+                    notifyIcon1.Visible = true;
+                }
+
+            try
+            {
+                RegistryKey k = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\LanAgentSettings");
+                tbxAdress.Text = (string)k.GetValue("IpAdress");
+                tbxPort.Text = (string)k.GetValue("Port");
+                tbxTime.Text = (string)k.GetValue("Time");
+                k.Close();
+            }
+            catch
+            {
+                tbxAdress.Text = "127.0.0.1";
+                tbxPort.Text = "9595";
+                tbxTime.Text = "10";
+            }
+            timer1_Tick(sender,e);
+        }
+
+        private void tbxTime_TextChanged(object sender, EventArgs e)
+        {
+            if (tbxTime.Text == "" || tbxTime.Text == "0")
+                timer1.Stop();
+            else
+            {
+                int a = Convert.ToInt32(tbxTime.Text) * 60000;
+                timer1.Interval = a;
+                timer1.Start();
+            }
+            
+        }
+
+        public void SetAutorun(string name ,bool autorun)
+        {
+            string exepath = Application.ExecutablePath;
+            RegistryKey k = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run");
+            try
+            {
+                if (autorun)
+                    k.SetValue(name, exepath + " /min");
+                else
+                    k.DeleteValue(name);
+                k.Close();
+            }
+            catch { }
+        }
+
+        private void cbxAutorun_CheckedChanged(object sender, EventArgs e)
+        {
+            SetAutorun("LanAgent", cbxAutorun.Checked);
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+            ShowInTaskbar = true;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if(WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                notifyIcon1.Visible = true;
+                this.ShowInTaskbar = false;
+            }
+        }
+    }
+}
